32259bbfd29395b84ea59dc0a34d64ccacb3dcd5
mcabe <mcabe@me.com>
2013-11-12 12:22:23 +0100

    (transfer and accounthist JSPs for cuntomer) added these commands in the factory... bitch

--- DailyBanking/src/java/servlets/Factory.java
+++ DailyBanking/src/java/servlets/Factory.java
@@ -36,7 +36,8 @@ public class Factory {
         commands.put("customerIndex", new CustomerIndexCommand("/customer/customerIndex.jsp", "Customer Index",SecurityRole.Customers));
         commands.put("customerViewDetails", new ViewCustomerCommand("/customer/customerViewDetails.jsp","Your Details",SecurityRole.Customers));
         commands.put("customerViewAccount", new ViewAccountCommand("/customer/customerViewAccount.jsp","Account Details",SecurityRole.Customers));
-        
+        commands.put("customerAccountHistory", new ShowAccountHistoryCommand("/customer/customerAccountHistory.jsp","Your Account History",SecurityRole.Customers));
+        commands.put("customerAddTransfer", new CreateTransferCommand("customer/customerViewAccount.jsp","Account Details",SecurityRole.Customers));
         
         //bankTeller
         commands.put("bankTellerIndex", new BankTellerIndexCommand("/bankTeller/bankTellerIndex.jsp", "BankTeller Index",SecurityRole.BankTellers));
