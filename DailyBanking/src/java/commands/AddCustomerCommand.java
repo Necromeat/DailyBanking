30a86ac521c9087d317410db76b08162fc2b98ad
Aaron <Aaron@10.18.132.84>
2013-11-26 10:13:07 +0100

    some thing new to bitch about

--- DailyBanking/src/java/commands/AddCustomerCommand.java
+++ DailyBanking/src/java/commands/AddCustomerCommand.java
@@ -1,14 +1,15 @@
-
 package commands;
 
 import DTO.CustomerDTO;
+import DTO.UserDTO;
 import javax.servlet.http.HttpServletRequest;
 import security.SecurityRole;
 import shared.Customer;
 
 /**
  *
- * @author Aaron, Kris, Lars, Timea, @08/11/2013
+ * @author Aaron, Kris, Lars, Timea,
+ * @08/11/2013
  */
 public class AddCustomerCommand extends TargetCommand {
 
@@ -16,15 +17,16 @@ public class AddCustomerCommand extends TargetCommand {
         super(target, title, role);
     }
 
-    
-
     @Override
     public String execute(HttpServletRequest request) {
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String email = request.getParameter("email");
+        String password = request.getParameter("password");
         if (!firstName.equals("")) {
-            CustomerDTO cust = new CustomerDTO(0,firstName, lastName, email);
+            UserDTO user = new UserDTO(email, 0, password);
+            servlets.Factory.getInstance().getBankController().addUser(user.getPw(), user.getEmail());
+            CustomerDTO cust = new CustomerDTO(0, firstName, lastName, email);
             servlets.Factory.getInstance().getBankController().addCustomer(cust);
             return super.execute(request);
         } else {
